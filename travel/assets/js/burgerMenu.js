c904cae3b3cb2766581e52d748a32d2ff0d5021e
Herman Kudria <h.kudrya@hotmail.com>
2022-06-20 13:50:26 +0200

    feat: add burger menu with js

---
+++ travel/assets/js/burgerMenu.js
@@ -0,0 +1,27 @@
+const openBurgerMenu = document.getElementById('openBurgerMenu')
+const closeBurgerMenu = document.getElementById('closeBurgerMenu')
+const burgerMenu = document.getElementById('burgerMenu')
+const body = document.getElementById('body')
+
+
+openBurgerMenu.addEventListener('click',() => {
+    body.classList.add('show')
+    window.setTimeout(showMenu,1)
+    function showMenu() {
+        burgerMenu.classList.add('open')
+    }
+})
+
+closeBurgerMenu.addEventListener('click',closeMenu)
+body.addEventListener('click',closeMenu)
+
+function closeMenu(event){
+    if(event.target.tagName === 'DIV' || event.target.tagName === 'IMG'){
+        burgerMenu.classList.remove('open')
+        window.setTimeout(showMenu,800)
+        function showMenu() {
+            body.classList.remove('show')
+        }
+    }
+}
+
